b13a07464b7adc1a92a3ee972d2165d092405447
mlabarinas <mariano.labarinas@mercadolibre.com>
2017-06-14 19:33:16 -0300

    Changes

--- go-talks/resources/src/06_pointers/main.go
+++ go-talks/resources/src/06_pointers/main.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	x := 4
 	fmt.Println("X Value:" + strconv.Itoa(x))
+
 	y := new(int)
 	fmt.Println("Y Value:" + strconv.Itoa(*y))
+
 	*y = 4
 	fmt.Println("Y Value:" + strconv.Itoa(*y))
+
 	if y == &x {
 		fmt.Println("X & Y are equals")
 	} else {
