8b58ef7310efd551f1d6caec334a8b1a3b026984
Gonzalo Saya <gonzalo.sayago@mercadolibre.com>
2019-09-18 13:47:02 -0300

    Update main.go

--- go-talks/public_workshop_2019_spring/resources/src/06_pointers/main.go
+++ go-talks/public_workshop_2019_spring/resources/src/06_pointers/main.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var p *int
+	var p *int	// Explícita
 	fmt.Println(p == nil)
 
-	y := new(int)
+	y := new(int)	// Explícita
 	fmt.Printf("Y:%p\n", y)
 	fmt.Printf("Y Value: %d\n", *y)
 
 	*y = 4
 	fmt.Printf("Y Value: %d\n", *y)
 
-	x := 4
+	x := 4		// Implícita
 	fmt.Printf("X Value: %d\n", x)
 
 	if y == &x {
