0922041b32a77e0efd0d30fd51c885d61262c13e
Gonzalo Saya <gonzalo.sayago@mercadolibre.com>
2019-09-18 14:12:39 -0300

    Update main.go

--- go-talks/public_workshop_2019_spring/resources/src/06_pointers/4/main.go
+++ go-talks/public_workshop_2019_spring/resources/src/06_pointers/4/main.go
@@ -6,17 +6,17 @@ func main() {
 	a := new(int)
 	*a = 10
 
-	fmt.Printf("Before method   -> pointer address: %v, value: %d \n", a, *a)
+	fmt.Printf("Before method -> pointer address: %v, value: %d \n", a, *a)
 	increment(a)
-	fmt.Printf("After method    -> pointer address: %v, value: %d \n", a, *a)
+	fmt.Printf("After method  -> pointer address: %v, value: %d \n", a, *a)
 }
 
 func increment(a *int) {
-	fmt.Printf("In inc method a -> pointer address: %v, value: %d \n", a, *a)
+	fmt.Printf("In inc a      -> pointer address: %v, value: %d \n", a, *a)
 	b := new(int)
 	*b = *a
 	*b++
 	a = b
-	fmt.Printf("In inc method a -> pointer address: %v, value: %d \n", a, *a)
-	fmt.Printf("In inc method b -> pointer address: %v, value: %d \n", b, *b)
+	fmt.Printf("In inc a      -> pointer address: %v, value: %d \n", a, *a)
+	fmt.Printf("In inc b      -> pointer address: %v, value: %d \n", b, *b)
 }
