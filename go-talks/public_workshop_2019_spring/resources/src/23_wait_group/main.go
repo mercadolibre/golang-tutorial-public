e12e519b2b6ec89f860f9b79ab1cb01272ce58e7
fgarciareyna <facundo.rgarcia@mercadolibre.com>
2019-09-24 16:32:26 -0300

    wait group

--- go-talks/public_workshop_2019_spring/resources/src/23_wait_group/main.go
+++ go-talks/public_workshop_2019_spring/resources/src/23_wait_group/main.go
@@ -5,17 +5,26 @@ import (
 	"sync"
 )
 
+// START OMIT
 func main() {
-
 	// Create a wait group.
 	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		fmt.Println("Hello gouroutines!")
-		wg.Done()
-	}()
+	wg.Add(3)
+	for i := 0; i < 3; i++ {
 
+		go func() {
+			defer wg.Done()
+			// Simple function call.
+			doJob()
+		}()
+	}
 	// Wait until everyone finishes.
 	wg.Wait()
 }
+
+func doJob() {
+	fmt.Println("Hello goroutines!")
+}
+
+//END OMIT
